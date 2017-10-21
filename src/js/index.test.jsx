0adf462d8aaca322a74494d58f1753b3a6da0608
Brenton Cozby <brenton.cozby@gmail.com>
2017-10-21 13:39:09 -0400

    get tests working

--- src/js/index.test.jsx
+++ src/js/index.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -7,21 +7,35 @@ import configStore from './store.js'
 import App from './components/app-component.jsx'
 
 describe('app-component', () => {
-    it('renders App with router and store without crashing', () => {
+    let stripeMockFn
+    let stripeMockResult
+
+    beforeEach(() => {
+        stripeMockResult = {
+            elements: jest.fn(),
+            createToken: jest.fn(),
+            createSource: jest.fn(),
+        }
+        stripeMockFn = jest.fn().mockReturnValue(stripeMockResult)
+        window.Stripe = stripeMockFn
+
+        process.env.perishable_key = 'TEST'
+    })
+    
+    test('renders App with router and store without crashing', () => {
+        const store = configStore()
         const reactStripeElements = jest.genMockFromModule('react-stripe-elements')
 
         reactStripeElements.StripeProvider = jest.fn()
 
-        const div = document.createElement('div')
-        const store = configStore()
-        
-        ReactDOM.render(
+        const wrappedApp = renderer.create(
             <Provider store={store}>
                 <Router>
                     <Route path={PP} component={App} />
                 </Router>
-            </Provider>,
-            div
+            </Provider>
         )
+
+        expect(wrappedApp.toJSON()).toMatchSnapshot()
     })
 })
